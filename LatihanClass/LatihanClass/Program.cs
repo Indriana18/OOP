49b692558b844ba59fa8e380ec807bc5532c4a9a
Indriana18 <48426491+Indriana18@users.noreply.github.com>
2019-04-09 15:01:22 +0700

    Add files via upload

---
+++ LatihanClass/LatihanClass/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time objTime = new Time();
+
+            objTime.Year = 2009;
+            objTime.Month = 5;
+            objTime.Date = 1;
+            //mengambil nilai dengan property
+            Console.WriteLine("Year : {0}", objTime.Year.ToString());
+            Console.WriteLine("Month : {0}", objTime.Month.ToString());
+            Console.WriteLine("Date : {0}", objTime.Date);
+        }
+    }
+}
---
+++ LatihanClass/LatihanClass/Time.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClass
+{
+    class Time
+    {
+        public int year;
+        public int month;
+        public int date;
+        public int hour;
+        public int minute;
+        public int second;
+        public int Year {
+            get { return year; }
+            set { year = value; }
+        }
+        public int Month {
+            get { return month; }
+            set { month = value; }
+        }
+        public int Date {
+            get { return date; }
+            set { date = value; }
+        }
+        public int Hour {
+            get { return hour; }
+            set { hour = value; }
+        }
+        public int Minute {
+            get { return minute; }
+            set { minute = value; }
+        }
+        public int Second {
+            get { return second; }
+            set { second = value; }
+
+        public Time()
+        {
+            year = 2018;
+            month = 10;
+            date = 10;
+            hour = 10;
+            minute = 30;
+            second = 40;
+        }
+
+    }
+    /*{
+        Console.WriteLine("Waktu sekarang : {0}", DateTime.Now.ToString());
+    }
+    */
+
+}
+}
